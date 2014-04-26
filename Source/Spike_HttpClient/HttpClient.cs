e901c50fa7338613606a5dcda7a14f3ca13de0d1
bala07 <sendmailtobalu@gmail.com>
2014-04-26 16:13:06 +0530

    Spiked http client and server communications - <Bala>

---
+++ Source/Spike_HttpClient/HttpClient.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Spike_HttpClient
+{
+    public class HttpClient
+    {
+        private string ServerUrl { set; get; }
+
+        public HttpClient(string serverUrl, int serverPort)
+        {
+            ServerUrl = serverUrl;
+        }
+
+        static void Main(string[] args)
+        {
+            var client = new HttpClient("http://localhost:8000?name=foo&password=bar", 8000);
+            client.Start();
+        }
+
+        public void Start()
+        {
+//            TalkToServer();
+            SendFileToServer();
+            Console.ReadLine();
+        }
+
+        private void TalkToServer()
+        {
+            var httpWebRequest = WebRequest.Create(new Uri(ServerUrl)) as HttpWebRequest;
+            httpWebRequest.Method = WebRequestMethods.Http.Get;
+
+            //            var parameters = GetParameters();
+            //            var urlParameters = PrepareUrlParameterList(parameters);
+
+            string dataFromServer = "";
+
+            try
+            {
+                var httpResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                Console.WriteLine("Reading from server's response");
+
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    dataFromServer = ReadResponseFromServer(httpResponse);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception in client:" + e.Message);
+                return;
+            }
+
+            Console.WriteLine(dataFromServer);
+
+            Console.ReadLine();
+        }
+
+        private string ReadResponseFromServer(HttpWebResponse httpResponse)
+        {
+            string responseMessage;
+            Console.WriteLine("Reading message from server");
+
+            using (var responseStream = httpResponse.GetResponseStream())
+            {
+                using (var reader = new StreamReader(responseStream))
+                {
+                    responseMessage = reader.ReadToEnd();
+                }
+            }
+
+            return responseMessage;
+        }
+
+        private void SendFileToServer()
+        {
+            const string fileToUpload = @"C:\Users\Rohini\My Documents\test.txt";
+            const string uploadUrl = "http://localhost:8000/";
+
+            var fileStream = new FileStream(fileToUpload, FileMode.Open);
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(uploadUrl);
+            httpWebRequest.Method = "POST"; // you might use "POST"
+            httpWebRequest.ContentLength = fileStream.Length;
+            httpWebRequest.AllowWriteStreamBuffering = true;
+
+            Stream requestStream = httpWebRequest.GetRequestStream();
+
+            var inData = new byte[fileStream.Length];
+
+            // Get data from upload file to inData 
+            fileStream.Read(inData, 0, int.Parse(fileStream.Length.ToString(CultureInfo.InvariantCulture)));
+
+            // put data into request stream
+            requestStream.Write(inData, 0, int.Parse(fileStream.Length.ToString(CultureInfo.InvariantCulture)));
+
+            fileStream.Close();
+
+            var response = httpWebRequest.GetResponse();
+
+            var messageFromServer = ReadResponseFromServer((HttpWebResponse)response);
+            Console.WriteLine(messageFromServer);
+
+            // after uploading close stream 
+            requestStream.Close();
+        }
+
+        private String PrepareUrlParameterList(IDictionary<string, string> parameters)
+        {
+            var urlParameters = new StringBuilder();
+
+            foreach (var parameter in parameters)
+            {
+                if (urlParameters.Length > 0)
+                {
+                    urlParameters.Append("&");
+                }
+
+                urlParameters.Append(parameter.Key + "=" + Uri.EscapeUriString(parameter.Value));
+            }
+
+            return urlParameters.ToString();
+        }
+
+        private IDictionary<string, string> GetParameters()
+        {
+            return new Dictionary<string, string>
+            {
+                { "Username", "foo"},
+                { "Password", "bar"}
+            };
+        }
+
+    }
+}
---
+++ Source/Spike_HttpServer/HttpServer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace Spike_HttpServer
+{
+    class HttpServer
+    {
+        private HttpListener httpListener;
+        private const string ServerUrl = "http://localhost:8000/";
+
+        static void Main()
+        {
+            var server = new HttpServer();
+            server.Start();
+
+        }
+
+        public void Start()
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add(ServerUrl);
+            httpListener.Start();
+
+            ListenForConnections();
+        }
+
+        private void ListenForConnections()
+        {
+            while (true)
+            {
+                var httpListenerContext = httpListener.GetContext();
+                var thread = new Thread(HandleClient);
+                thread.Start(httpListenerContext);
+            }
+        }
+
+        private void HandleClient(object obj)
+        {
+            var httpListenerContext = (HttpListenerContext) obj;
+
+            ReadFileFromClient(httpListenerContext);
+
+            WriteToClient(httpListenerContext.Response, "You are connected and your file has been received!");
+        }
+
+        private void ReadFileFromClient(HttpListenerContext context)
+        {
+            using (var stream = context.Request.InputStream)
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var data = reader.ReadToEnd();
+                    Console.WriteLine(data);
+                }
+            }
+
+            Console.WriteLine("Finished reading the file from client");
+        }
+
+        private void WriteToClient(HttpListenerResponse httpResponse, string responseString)
+        {
+            byte[] responseContents = Encoding.UTF8.GetBytes(responseString);
+
+            httpResponse.ContentLength64 = responseContents.Length;
+
+            using (var outputStream = httpResponse.OutputStream)
+            {
+                outputStream.Write(responseContents, 0, responseContents.Length);
+            }
+        }
+    }
+}
