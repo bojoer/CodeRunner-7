1b45ec83886f4a4652e41ed940e164b7b6ce71b7
bala07 <sendmailtobalu@gmail.com>
2014-04-26 16:07:32 +0530

    Added constants as a separate directory in the solution - <Bala>

---
+++ Source/Master/Constants/EnvironmentConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace MasterServer.Constants
+{
+    static class EnvironmentConstants
+    {
+        public const string ServerIp = "127.0.0.1";
+
+        public const int ServerPort = 8000;
+
+        public static string FtpUrl = "ftp://127.0.0.1/%2E%2E";
+    }
+}
