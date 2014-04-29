794816a0eb77d3dd0ccd1f7f438e5ea1a6501c40
Balasubramanian N <balasubn@thoughtworks.com>
2014-04-29 16:54:28 +0530

    Testing the running of code programmatically - <Bala>

---
+++ Source/Spike_RunningCode/Runner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+
+using Microsoft.CSharp;
+
+namespace Spike_RunningCode
+{
+    public class Runner
+    {
+        static void Main(string[] args)
+        {
+            var runner = new Runner();
+
+            var compilerResults = runner.Compile(@"C:\Projects\CodeRunner\TestClass.cs");
+
+            runner.Run(compilerResults);
+
+            Console.ReadLine();
+        }
+
+        public CompilerResults Compile(string filePath)
+        {
+            var parameters = new CompilerParameters
+                                 {
+                                     GenerateInMemory = true,
+                                     TreatWarningsAsErrors = false,
+                                     GenerateExecutable = false,
+                                     CompilerOptions = "/optimize"
+                                 };
+
+            var codeProvider = new CSharpCodeProvider();
+            var compilerResults = codeProvider.CompileAssemblyFromFile(parameters, new[] { filePath });
+
+            return compilerResults;
+        }
+
+        public void Run(CompilerResults compilerResults)
+        {
+            if (compilerResults.Errors.HasErrors)
+            {
+                string errors = compilerResults.Errors.Cast<object>().Aggregate("Compilation errors: ", (current, error) => current + ("rn" + error));
+                Console.WriteLine(errors);
+                Console.ReadLine();
+                return;
+            }
+
+            var module = compilerResults.CompiledAssembly.GetModules()[0];
+            var moduleType = module.GetType("TestClass");
+            var methodInfo = moduleType.GetMethod("Add");
+
+            var returnValue = (int) methodInfo.Invoke(Activator.CreateInstance(moduleType), new object[] { 1, 2 });
+            Console.WriteLine(returnValue);
+        }
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class TestClass
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello world!");
+	}
+
+	public int Add(int a, int b)
+	{
+		return a + b;
+	}
+}
